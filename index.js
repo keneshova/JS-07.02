f10b419f1cc9f53f6d6d45e0151c5da0314f5f12
Nestan <nestankeneshova16@gmail.com>
2023-02-07 14:34:41 +0600

    added a loop that counts from 5 to 25

---
+++ index.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+  let result = a + b;
+ console.log(result)
+}
+let m = sum(5,6);
+
+let myName = "Nestana";
+console.log(myName.length);//сколько букв
+
+//цикл который посчитает от 5 до 25
+let number = 5;
+while (number <= 25){
+  console.log (number);
+  number++;
+}
